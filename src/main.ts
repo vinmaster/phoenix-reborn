9f8a9c44daaf7c660414b46b1a72139a987847fc
Vincent Ho <vinmaster99@gmail.com>
2023-08-31 08:12:16 -0700

    init commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import './style.css';
+import 'phaser';
+
+const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'ExampleGame',
+  url: 'https://github.com/digitsensitive/phaser3-typescript',
+  version: '2.0',
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: 'app',
+  // `as as Phaser.Types.Scenes.SettingsConfig[]` is required until https://github.com/photonstorm/phaser/pull/6235
+  // scene: [menu()] as Phaser.Types.Scenes.SettingsConfig[],
+  input: {
+    keyboard: true,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  backgroundColor: '#300000',
+  render: { pixelArt: false, antialias: true },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    // `fullscreenTarget` must be defined for phones to not have
+    // a small margin during fullscreen.
+    fullscreenTarget: 'app',
+    expandParent: false,
+  },
+};
+
+export class Game extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener('load', () => {
+  // Expose `_game` to allow debugging, mute button and fullscreen button
+  (window as any)._game = new Game(GameConfig);
+});
